7fbc82f108d13563d1d0d046a8a642b770fdf727
Jukka Nikki <jukka.nikki@kolumbus.fi>
2023-05-16 08:25:07 +0200

    initial generated version

---
+++ android/app/src/main/kotlin/fi/nikkijuk/blocgorouter/MainActivity.kt
@@ -0,0 +1,6 @@
+package fi.nikkijuk.blocgorouter.blocgorouter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:blocgorouter/app/app.dart';
+import 'package:blocgorouter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
