46ba422d405525d126580d402dce086b1e5839aa
Jukka Nikki <jukka.nikki@kolumbus.fi>
2023-05-21 21:36:25 +0200

    linter & formatter are happy again

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,21 +1,19 @@
 import 'package:blocgorouter/app/app.dart';
 import 'package:blocgorouter/config/firebase_config.dart';
 import 'package:blocgorouter/counter/counter.dart';
-import 'package:blocgorouter/features/authentication/view/user_signin_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      initFirebase();
+      await initFirebase();
 
       await tester.pumpWidget(const App());
 
       // router redirect brings user to login page
       expect(find.byType(CounterPage), findsNothing);
       //expect(find.byType(UserSignInPage), findsOneWidget);
-    }, skip: true);
-
+    }, skip: true,
+    );
   });
 }
